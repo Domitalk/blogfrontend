9d74157fc5f318ea8dc5d96661a7e53d589a8718
domitalk <dominic.m.chu@gmail.com>
2020-06-24 21:03:44 -0400

    Updates

---
+++ precache-manifest.9e76f5689a0f7ac53d0831d5c0365cec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c91651d0be4521d9bb31a9425336d66",
+    "url": "/blogfrontend/index.html"
+  },
+  {
+    "revision": "2e6c9a98f05ad5d818f2",
+    "url": "/blogfrontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e2510626939f4b6d898f",
+    "url": "/blogfrontend/static/js/2.edcfb48b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blogfrontend/static/js/2.edcfb48b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e6c9a98f05ad5d818f2",
+    "url": "/blogfrontend/static/js/main.d31f2cef.chunk.js"
+  },
+  {
+    "revision": "a441d919f82ec623ac25",
+    "url": "/blogfrontend/static/js/runtime-main.6a8d9d50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blogfrontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
